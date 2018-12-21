adeb88fd5da5f3f7c2954269c4bb7120ef204c44
tangxin <tangxin@tangxin-mbp>
2018-12-21 15:35:36 +0800

    add hpa

--- concept/ingress/ingress-controller/aliyun-nginx-ingress-launcher/installer.sh
+++ concept/ingress/ingress-controller/aliyun-nginx-ingress-launcher/installer.sh
@@ -8,13 +8,19 @@ cd $(dirname $0)
 IngressClass=inner-nginx
 NAMESPACE=ingress-system--${IngressClass}
 
-LoadbalancerID=lb-2zehwyt3p9wx27z3pp5lf
+LoadbalancerID=lb-bp1wrb6j0l9kpnux73i1t
 
 mkdir -p ${IngressClass}
 jinja2 -D Namespace="${NAMESPACE}" \
        -D LoadbalancerID=${LoadbalancerID} \
        -D IngressClass=${IngressClass} \
        ingress-controller-template.yml.j2 > ${IngressClass}/ingress-controller.yml
+
+jinja2 -D Namespace="${NAMESPACE}" \
+       -D LoadbalancerID=${LoadbalancerID} \
+       -D IngressClass=${IngressClass} \
+       ingress-controller-template-hpa.yml.j2 > ${IngressClass}/ingress-controller-hpa.yml
+
 
 
 ## demo
