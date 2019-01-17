9d8c4ab4225df1e6cd6e0376d5aff9a7efde83ed
tangxin <tangxin@tangxin-mbp>
2019-01-17 16:38:09 +0800

    update: 按照 function 更新 nfs-client installer.sh

--- concept/storageclass/nfs/nfs-client/nfs-client-launcher/installer.sh
+++ concept/storageclass/nfs/nfs-client/nfs-client-launcher/installer.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-export NFS_SERVER=192.168.100.200
+cd $(dirname $0)
+
+export NFS_SERVER=192.168.1.200
 export NFS_PATH=/kubernetes
 
 export NAMESPACE=storage-system
 
-export PROJECT_NAME=qcloud-nfs-client-provisioner
-export PROVISIONER_NAME=qcloud/shanghai-nas-ssd
-
-export STORAGECLASS_NAME=qcloud-shanghai-nas-ssd 
+export IDC=aliyun
+export STORAGECLASS_NAME=${IDC}-hangzhou-nfs-ssd
 export STORAGECLASS_ArchiveOnDelete=false  # true or false
+
+export PROJECT_NAME=${STORAGECLASS_NAME}-client-provisioner
+export PROVISIONER_NAME=${IDC}/hangzhou-nfs-ssd
+
 
 export PVC_NAME=test-claim-44
 
 mkdir -p $PROJECT_NAME
 
-which envsubst || exit 1
+which envsubst >/dev/null 2>&1 || exit 1
 
+function storageclass(){
 ## nfs-client-provisioner
 envsubst < rbac.yaml.tpl > ${PROJECT_NAME}/rbac.yaml
 envsubst < deployment-linux.yaml.tpl > ${PROJECT_NAME}/deployment-linux.yaml
 # envsubst < deployment-arm.yaml.tpl > ${PROJECT_NAME}/deployment-arm.yaml
 envsubst < storageclass.yaml.tpl > ${PROJECT_NAME}/storageclass.yaml
+}
 
+
+function client(){
 # test
+
+export PVC_NAME=aliyun-nas-pvc01
+# export STORAGECLASS_NAME=aliyun-beijing-nas-ssd
 envsubst < test-nfs-pvc.yaml.tpl > ${PROJECT_NAME}/test-nfs-pvc.yaml
 envsubst < test-nfs-pod.yaml.tpl > ${PROJECT_NAME}/test-nfs-pod.yaml
+}
 
+storageclass
+client
